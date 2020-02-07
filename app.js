94db30e1ec694f81365c52db10ec0cbf2cfcfce6
marcksevan <58758285+marcksevan@users.noreply.github.com>
2020-02-06 23:17:11 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links a');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
