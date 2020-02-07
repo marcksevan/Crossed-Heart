e2cb339385d8286ab7486da2a9fbff61e5aa2920
marcksevan <58758285+marcksevan@users.noreply.github.com>
2020-02-06 23:53:20 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links a');
 
   burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
-  });
+
+    burger.classList.toggle('toggle');
+    });
 }
 
 navSlide();
